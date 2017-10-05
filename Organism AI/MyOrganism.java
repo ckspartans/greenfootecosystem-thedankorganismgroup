2d0ef7807bb2d0b80ac563e2e333d37758b48353
Uzair Ahmed <uzairmahmed@gmail.com>
2017-10-05 01:05:12 -0400

    Test Commit 1
    Got stuff moving, maybe tracking. Don't know how to check if its tracking yet.

--- Organism AI/MyOrganism.java
+++ Organism AI/MyOrganism.java
@@ -1,129 +1,120 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
 
 /**
  * Extends off AbstOrganism to create a basic organism that reproduces, and moves around the screen.
- * 
- * CHANGELOG September 30, 2017 
+ *
+ * CHANGELOG September 30, 2017
  *      - Added variables to complement that of AbstOrganism 0.0.5
- * 
+ *
  * @author Uzair Ahmed
  * @version 0.4
  */
 
-public class MyOrganism extends AbstOrganism
-{
-    protected int speed = 5;
-    protected int organismX; //X location (can be protected
-    protected int organismY;//Y location (can be protected
-    public MyOrganism(){
-        ArrayList <Object> lifeforms;       //list of all the organsims in the game
-        ArrayList <Object> prey;            //list of all that the types of organism can feed on
-        ArrayList <Object> predators;       //list of all the types of organsims that the organism can be eaten 
-        
-        // Custom Variables
-        int startingMaxHealth = 10;
-        int startingMaxXp = 10;
-        int startingSpeed = 5;
-        int startingAttackPower = 1;
-        int startingDefensePower = 1;
-        int maxBuyableMaxHealth = 100;
-        int maxBuyableMaxXp = 100;
-        int maxBuyableSpeed = 10;
-        int maxBuyableAtt = 10;
-        int maxBuyableDef = 10;
-        
-        //XP Upgradeable Variables
-        int maxHealth = startingMaxHealth; //Maximum Health
-        int maxXp = startingMaxXp; //Max XP Storage
-        //int speed = startingSpeed; //Nuff Said 
-        int att = startingAttackPower; //Attack power
-        int def = startingDefensePower; //Defensive power
-        
-        //"Current" Variables. (These are live variables that are constantly changing.
-        int age = 0; //Time
-        int health = startingMaxHealth; //Out of maxHealth
-        int xp = 0; //Out of maxXp
-        int radius = health; //Same as value from health
-        Color playerColor = new Color(0,0,255); //Same as value mapped to 255.
-        //int organismX = 0;
-        //int organismY = 0;
+public class MyOrganism extends AbstOrganism {
 
-        MainWorld world;        
-    }
-    
-    public void act() 
-    {
-        
-        move();
-        consumeFood();
-        consumePlayer();
-        reproduce();
-        age();
+  public MyOrganism() {
 
-    }    
-    
-    protected void move(){
-        if(Greenfoot.isKeyDown("w")){
-            organismY = - speed;
-        }
-        else if (Greenfoot.isKeyDown("s")){
-            organismY = speed;
-        }
-        else{
-            organismY = 0;
-        }
-        if (Greenfoot.isKeyDown("a")){
-            organismX = - speed;
-        }
-        else if (Greenfoot.isKeyDown("d")){
-            organismX = speed;
-        }
-        else{
-            organismX = 0;
-        }
-        setLocation(getX()+organismX, getY() + organismY);
+    // Custom Variables
+
+
+    //XP Upgradeable Variables
+    int maxHealth = gvars.startingMaxHealth; //Maximum Health
+    int maxXp = gvars.startingMaxXp; //Max XP Storage
+    int speed = gvars.startingSpeed; //Nuff Said
+    int att = gvars.startingAttackPower; //Attack power
+    int def = gvars.startingDefensePower; //Defensive power
+    int sight = gvars.startingSight; //Sight
+
+    //"Current" Variables. (These are live variables that are constantly changing.
+    int age = 0; //Time
+    int health = gvars.startingMaxHealth; //Out of maxHealth
+    int xp = 0; //Out of maxXp
+    int radius = health; //Same as value from health
+    Color playerFill = new Color(0, 0, 0); //Same as value mapped to 255.
+    Color playerBorder = new Color(0, 0, 255);
+    //int organismX = 0;
+    //int organismY = 0;
+
+    MainWorld world;
+  }
+
+  public void act() {
+
+    moveAround();
+    drawRadius(35,35,500);
+    //consumeFood();
+    //consumePlayer();
+    //reproduce();
+    //age();
+  }
+
+  public void moveAround() {
+    //drawRadius(getX(),getY(),sight);
+    List foodNearby = getObjectsInRange(sight, Food.class);
+    if ((foodNearby.size()) > 0){
+      track(foodNearby);
+      move(gvars.startingSpeed);
     }
-    
-    public void consumeFood(){
-        if (getOneIntersectingObject(Food.class)!= null){
-          //world.foodEaten ++;
-          //world.removeObject(); //Try and find a way to remove the object it touches
-          //call grow (or add it to a temp health variable) to increase health
-          //add xp
-        }
-        
+    else{
+      move(gvars.startingSpeed);
+      if (Greenfoot.getRandomNumber(100) < 25){
+        turn(Greenfoot.getRandomNumber(90)-45);
+      }
     }
-    
-    public void consumePlayer(){
-        if (getOneIntersectingObject(TestOrganism.class)!=null){ //Change testOrganism to enemyOrganism
-            //do stuff to return damage int to enemy and to self.
-            //add xp
-        }
-        
+    removeTouching(Food.class);
+    if (isAtEdge()){
+      turn(180);
     }
-    
-    public void shrink(int damageTaken){
-        //takes damagetaken and applys it to health and radius
+  }
+
+  public void track(List l){
+    Food nearest = (Food) l.get(0);
+    turnTowards(nearest.getX(),nearest.getY());
+  }
+
+  public void drawRadius(int x,int y,int sight){
+    GreenfootImage img = new GreenfootImage(sight,sight);
+    img.setColor(new Color(0,0,0));
+    img.drawOval(x,y,sight,sight);
+  }
+
+  public void consumeFood() {
+    if (getOneIntersectingObject(Food.class) != null) {
+
     }
-    
-    public void grow(int damageGiven){
-        //takes damageGiven and applys it to health and radius
+
+  }
+
+  public void consumePlayer() {
+    if (getOneIntersectingObject(TestOrganism.class) != null) { //Change testOrganism to enemyOrganism
+      //do stuff to return damage int to enemy and to self.
+      //add xp
     }
-    
-    public void age(){
-        //start a timer in act() and use that value to edit age value, and color
-    }  
-    
-    public void reproduce(){
-        //create new myOrganism object and do magic
-        //remove xp
-        //drop in size
-    }
-    
-    public void die(){
-        //Remove the object.
-        //disperse as food througout the world
-    }
-    
+
+  }
+
+  public void shrink(int damageTaken) {
+    //takes damagetaken and applys it to health and radius
+  }
+
+  public void grow(int damageGiven) {
+    //takes damageGiven and applys it to health and radius
+  }
+
+  public void age() {
+    //start a timer in act() and use that value to edit age value, and color
+  }
+
+  public void reproduce() {
+    //create new myOrganism object and do magic
+    //remove xp
+    //drop in size
+  }
+
+  public void die() {
+    //Remove the object.
+    //disperse as food througout the world
+  }
+
 }
--- Organism AI/TestOrganism.java
+++ Organism AI/TestOrganism.java
@@ -1,44 +1,40 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class TestOrganism here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class TestOrganism extends Actor
-{
-    protected int speed = 5;
-    protected int spX = 0;
-    protected int spY = 0;
-    /**
-     * Act - do whatever the TestOrganism wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.  
-        checkKeys();
-    }    
-    protected void checkKeys(){
-        if(Greenfoot.isKeyDown("w")){
-            spY = - speed;
-        }
-        else if (Greenfoot.isKeyDown("s")){
-            spY = speed;
-        }
-        else{
-            spY = 0;
-        }
-        if (Greenfoot.isKeyDown("a")){
-            spX = - speed;
-        }
-        else if (Greenfoot.isKeyDown("d")){
-            spX = speed;
-        }
-        else{
-            spX = 0;
-        }
-        setLocation(getX()+spX, getY() + spY);
+public class TestOrganism extends Actor {
+  protected int speed = 5;
+  protected int spX = 0;
+  protected int spY = 0;
+
+  /**
+   * Act - do whatever the TestOrganism wants to do. This method is called whenever
+   * the 'Act' or 'Run' button gets pressed in the environment.
+   */
+  public void act() {
+    // Add your action code here.
+    checkKeys();
+  }
+
+  protected void checkKeys() {
+    if (Greenfoot.isKeyDown("w")) {
+      spY = -speed;
+    } else if (Greenfoot.isKeyDown("s")) {
+      spY = speed;
+    } else {
+      spY = 0;
     }
+    if (Greenfoot.isKeyDown("a")) {
+      spX = -speed;
+    } else if (Greenfoot.isKeyDown("d")) {
+      spX = speed;
+    } else {
+      spX = 0;
+    }
+    setLocation(getX() + spX, getY() + spY);
+  }
 }
