2259329392aeb7019ed986227edbf97c8b194013
Unknown <emg01@the-gales.ca>
2017-11-01 07:56:24 -0400

    Organism Mutation
    ~Fixed problems
    ~Set up rare mutations

--- Organism AI/Mutation.java
+++ Organism AI/Mutation.java
@@ -8,24 +8,31 @@ import java.util.*;
  */
 public class Mutation{
     public static void mutate(Organism o){
-        if (o.xp%10 == 0){
-            if (Greenfoot.getRandomNumber(5) == 1){
+        if (o.xp%10 == 0){ //reproduce only while XP is divisible by ten
+            if (Greenfoot.getRandomNumber(50) == 1){ //repro chance
                 o.reproduce();
-                int chosenMutation = Greenfoot.getRandomNumber(1000);
-                if (chosenMutation <= 250){
+                int chosenMutation = Greenfoot.getRandomNumber(1000); //choose mutation
+                if (chosenMutation <= 250){ //attack
                     o.att += 2;
                 }
-                else if((chosenMutation > 250) && (chosenMutation <= 500)){
-                    o.def += 1;
+                else if((chosenMutation > 251) && (chosenMutation <= 500)){ //tankiness
+                    o.def += 2;
                     o.maxHealth += 5;
+                    o.speed -=1; //the tankier you are, the slower you are
                 }
-                else if((chosenMutation > 500) && (chosenMutation <= 750)){
+                else if((chosenMutation > 501) && (chosenMutation <= 750)){ //speed
                     o.speed += 2;
                 }
-                else if((chosenMutation > 750) && (chosenMutation <= 1000)){
+                else if((chosenMutation > 751) && (chosenMutation <= 1000)){ //sight range
                     o.sight += 10;
                 }
             }
         }
     }
+    
+    public static void mutate(Parasite p){
+        
+    }
+        
+    
 }
